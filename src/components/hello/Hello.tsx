595fa16adfbfa4ac12016c20a988f3dc08d99e27
0xMahabub <mahabub212002072@gmail.com>
2022-10-19 02:01:53 +0600

    feature/Routerv6

--- src/components/hello/Hello.tsx
+++ src/components/hello/Hello.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
 import './hello.scss'
 
 const Hello = () => {
-  return <h1>Hello Vite+React app!</h1>
+  return (
+    <header>
+      <h1>Hello Vite+React app!</h1>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/404">404</Link>
+        </li>
+      </ul>
+    </header>
+  )
 }
 
 export default Hello
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const Notfound: FC = () => {
+  return <h1>Page not found!</h1>
+}
+
+export default Notfound
